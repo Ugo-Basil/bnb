4fb0cc6f36a1a9c44b549372e6b4f8d889557212
Basil <ohuruogubasil@gmail.com>
2023-04-01 12:53:43 +0100

    feat:navbar/auth

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,27 @@
+
+'use client'
+
+import Image from "next/image"
+
+interface AvatarProps {
+    src: string | undefined | null;
+    size?: number;
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+    src,
+    size = 40
+}) => {
+    return (
+        <div className="relative">
+
+            <Image
+                src={src || "/images/placeholder.jpg"}
+                width={size}
+                height={size}
+                className="rounded-full" alt={""} />
+        </div>
+    )
+}
+
+export default Avatar
---
+++ app/components/modals/LoginModal.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { signIn } from 'next-auth/react';
+import {
+    FieldValues,
+    SubmitHandler,
+    useForm
+} from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+
+import Modal from "./Modal";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
+import Button from "../Button";
+
+
+
+const LoginModal = () => {
+  return (
+    <div>LoginModal</div>
+  )
+}
+
+export default LoginModal
