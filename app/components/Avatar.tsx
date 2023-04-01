5a2b669925ed699be17fa9c0d58ad74e5572755f
Basil <ohuruogubasil@gmail.com>
2023-04-01 12:58:22 +0100

    feat:navbar/auth

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,27 +1,24 @@
+"use client";
 
-'use client'
-
-import Image from "next/image"
+import Image from "next/image";
 
 interface AvatarProps {
-    src: string | undefined | null;
-    size?: number;
+  src: string | undefined | null;
+  size?: number;
 }
 
-const Avatar: React.FC<AvatarProps> = ({
-    src,
-    size = 40
-}) => {
-    return (
-        <div className="relative">
+const Avatar: React.FC<AvatarProps> = ({ src, size = 40 }) => {
+  return (
+    <div className="relative">
+      <Image
+        src={src || "/images/placeholder.jpg"}
+        width={size}
+        height={size}
+        className="rounded-full"
+        alt={""}
+      />
+    </div>
+  );
+};
 
-            <Image
-                src={src || "/images/placeholder.jpg"}
-                width={size}
-                height={size}
-                className="rounded-full" alt={""} />
-        </div>
-    )
-}
-
-export default Avatar
+export default Avatar;
